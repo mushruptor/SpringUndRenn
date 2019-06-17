d6ab155afd26695c9a79e35941b2cc6d00cdf3d4
mushruptor <marcel.legler@tu-dresden.de>
2019-06-17 23:20:34 +0200

    Added Blocktypes and Sprites
    Every Gameobject is now viewed as a sprite. There is an example of
    creating a player and a Blockobject and adding them onto an Updatelist.

--- src/game.py
+++ src/game.py
@@ -43,46 +43,86 @@ class Loader:
 
         return leveldata
 
-class GameObject:
-    def __init__(self, path):
-        self.imagepath = path
+class GameObject(pygame.sprite.Sprite):
+    def __init__(self, path, pos=Position(0,0)):
+        pygame.sprite.Sprite.__init__(self)
+        self.imagepath = IMAGEDIR + path
+        self.load_image()
+
+        #set the initial position of the object
+        self.rect = self.image.get_rect()
+        self.set_position(pos)
 
     def load_image(self):
+        # if you want animated pictures, load a list of images and iterate
+        # through them each frame you call the update method of an object which
+        # derived from this class
         self.image = pygame.image.load(self.imagepath).convert()
 
-    def set_position(self, Pos):
-        self.Pos = Pos
+    def set_position(self, pos):
+        self.rect.x = pos.X
+        self.rect.y = pos.Y
 
-    def set_size(self, Size):
-        self.Size
+    def set_size(self, size):
+        self.Size = size
 
-class Player(GameObject):
-    direction = Direction.NEUTRAL
-    speed = 10
+class Block(GameObject):
+    def __init__(self, path, pos, left=1, right=1, top=1, bottom=1):
+        GameObject.__init__(self, path, pos)
 
-    def __init__(self, StartPos):
-        GameObject.__init__(self, IMAGEDIR + "jumper.png")
-        self.Pos = StartPos
+        # tells if the object is solid in one or more directions. 1 marks solid
+        # and 0 marks non-solid
+        self.LEFT = left
+        self.RIGHT = right
+        self.TOP = top
+        self.BOTTOM = bottom
 
-#TODO Movement specified in separate class to be inherited from
+class Stone(Block):
+    def __init__(self, pos):
+        Block.__init__(self, "stone.png", pos)
+
+# movement interface
+class Movement:
     def move_right(self):
-        self.Pos.X = self.Pos.X + self.speed
+        self.rect.x = self.rect.x + self.speed
         self.direction = Direction.RIGHT
 
     def move_left(self):
-        self.Pos.X = self.Pos.X - self.speed
+        self.rect.x = self.rect.x - self.speed
         self.direction = Direction.LEFT
 
     def move_up(self):
-        self.Pos.Y = self.Pos.Y - self.speed
+        self.rect.y = self.rect.y - self.speed
         self.direction = Direction.UP
 
     def move_down(self):
-        self.Pos.Y = self.Pos.Y + self.speed
+        self.rect.y = self.rect.y + self.speed
         self.direction = Direction.DOWN
 
-    def update(self):
-        pass #Part with the jumpy jumpy thing
+# the player object
+class Player(GameObject,Movement):
+    def __init__(self, startpos, direction=Direction.NEUTRAL, speed=10, velocity=5):
+        GameObject.__init__(self, "jumper.png")
+        self.rect.x = startpos.X
+        self.rect.y = startpos.Y
+        self.speed = speed
+        self.direction = direction
+        self.velocity = velocity
+
+    def update(self, keys):
+        if (keys[K_d]):
+            self.move_right()
+        if (keys[K_a]):
+            self.move_left()
+        if (keys[K_w]):
+            self.move_up()
+        if (keys[K_s]):
+            self.move_down()
+        if (keys[K_SPACE]):
+            #self.player.jump()
+            pass
+
+        # insert gravity here
 
 class Window:
     def __init__(self, width, height):
@@ -97,34 +137,55 @@ class App:
 
         self.loader = Loader()
         self.window = Window(800,600)
-        self.player = Player(Position(20,20))
+
+        # list to keep track of alle the objects, which have to be updated
+        self.active_gameobjects = pygame.sprite.Group()
+        self.player_gameobjects = pygame.sprite.Group()
+        # don't have to be updated every iteration
+        self.passive_gameobjects = pygame.sprite.Group()
 
     def on_init(self):
+        #TODO remove: just to test the leveldata
         self.leveldata = self.loader.load_level(LEVELDIR + 'level1/')
         print(self.leveldata)
 
         pygame.init()
         self._display_surf = pygame.display.set_mode((self.window.width,self.window.height),pygame.HWSURFACE)
-
         pygame.display.set_caption('SpringUndRenn')
-        self._running = True
-        self.player.load_image()
-        self._image_surf = self.player.image
+
+        # create the player
+        player = Player(Position(20,20))
+        player.load_image() # remove this later
+        self.player_gameobjects.add(player)
+
+        # create some dummy stones
+        stone = Stone(Position(200,200))
+        stone.load_image() # remove this later
+        self.passive_gameobjects.add(stone)
+
+        self.clock = pygame.time.Clock()
 
     def on_event(self, event):
         if event.type == QUIT:
             self._running = False
 
+    # additional action which occurs each loop
     def on_loop(self):
-        pass
+        self.player_gameobjects.update(self.keys)
+        self.active_gameobjects.update()
 
+    # rendering the frame
     def on_render(self):
         self._display_surf.fill((0,0,0))
-        self._display_surf.blit(self._image_surf,(self.player.Pos.X,self.player.Pos.Y))
-        self.player.update()
-        pygame.display.flip()
-        sleep(0.03) #TODO implement Framerates
 
+        self.player_gameobjects.draw(self._display_surf)
+        self.active_gameobjects.draw(self._display_surf)
+        self.passive_gameobjects.draw(self._display_surf)
+
+        pygame.display.flip()
+        self.clock.tick(30)
+
+    # actions on shutting the program down
     def on_cleanup(self):
         pygame.quit()
 
@@ -134,25 +195,9 @@ class App:
 
         while(self._running):
             pygame.event.pump()
-            keys = pygame.key.get_pressed()
+            self.keys = pygame.key.get_pressed()
 
-            if (keys[K_d]):
-                self.player.move_right()
-
-            if (keys[K_a]):
-                self.player.move_left()
-
-            if (keys[K_w]):
-                self.player.move_up()
-
-            if (keys[K_s]):
-                self.player.move_down()
-
-            if (keys[K_SPACE]):
-                #self.player.jump()
-                pass
-
-            if (keys[K_ESCAPE]):
+            if (self.keys[K_ESCAPE]):
                 self._running = False
 
             self.on_loop()
